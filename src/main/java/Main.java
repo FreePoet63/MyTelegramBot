8661b2622af393891a0b43aaaf06da9c4a9faf51
FreePoet63 <66965157+FreePoet63@users.noreply.github.com>
2023-07-19 23:04:34 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,3 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.methods.*;
-import org.apache.http.impl.client.*;
-
-import java.io.*;
-
 public class Main {
-    public static void main(String[] args) {
-    }
+    
 }
