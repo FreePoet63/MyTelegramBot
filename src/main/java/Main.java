8af9763c03dd3c3adc5b579ec7df4e18ca6e1998
FreePoet63 <66965157+FreePoet63@users.noreply.github.com>
2023-07-19 23:01:33 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,29 +6,6 @@ import org.apache.http.impl.client.*;
 import java.io.*;
 
 public class Main {
-    public static final String url = "https://api.nasa.gov/planetary/apod?api_key=ВАШ КЛЮЧ";
-    public static final ObjectMapper mapper = new ObjectMapper();
-
-    public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
-                .setDefaultRequestConfig(RequestConfig.custom()
-                        .setConnectTimeout(5000)
-                        .setSocketTimeout(30000)
-                        .setRedirectsEnabled(false)
-                        .build())
-                .build();
-
-        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
-
-        NasaObject nasaObject = mapper.readValue(response.getEntity().getContent(), NasaObject.class);
-        System.out.println(nasaObject);
-
-        CloseableHttpResponse imageResponse = httpClient.execute(new HttpGet(nasaObject.getUrl()));
-        String[] array = nasaObject.getUrl().split("/");
-        String fileName = array[array.length - 1];
-
-        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
-        imageResponse.getEntity().writeTo(fileOutputStream);
-        fileOutputStream.close();
+    public static void main(String[] args) {
     }
 }
